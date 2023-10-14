b2dcde4be82bdd0598af641ce7fd5d22626f7059
mukilan2815 <mukilan2808@gmail.com>
2023-10-14 08:16:27 +0530

    third

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import 
-
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Aboutus from "./components/Aboutus";
+import Faculties from "./components/Faculties";
+import Ourservices from "./components/Ourservices";
+import Ourworks from "./components/Ourworks";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Home />
+      <Aboutus />
+      <Faculties />
+      <Ourservices />
+      <Ourworks />
+      <Footer />
     </div >
   );
 }
