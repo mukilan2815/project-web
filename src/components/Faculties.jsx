d4e33f5b9c4eb8a0d69e49c7f7e7720df9bd3c42
mukilan2815 <mukilan2808@gmail.com>
2023-10-14 10:22:56 +0530

    fifth

--- src/components/Faculties.jsx
+++ src/components/Faculties.jsx
@@ -7,18 +7,7 @@ const Faculties = () => {
         <h1 className="font-semibold my-14">FACULTIES</h1>
       </div>
       <div className="flex justify-around flex-wrap">
-        <Facultycard
-          title="Dr.A.AMUDHA,Ph.D"
-          description="Head & Dean"
-        />
-        <Facultycard
-          title="Dr.V.J.VIJAYALAKSHMI,Ph.D"
-          description="Certified Energy Manager"
-        />
-        <Facultycard
-          title="Mrs.P.NAGAVENI,M.E"
-          description="Certified Energy Manager"
-        />
+        <Facultycard />
       </div>
       <br />
       <br />
