9acba27b67fb6ec0908ca9d260e46b302bff4139
mukilan2815 <mukilan2808@gmail.com>
2023-10-16 10:33:56 +0530

    changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,18 +7,20 @@ const Home = () => {
       <span className="absolute z-0">
         <Bgpic />
       </span>
-      <div className="text-3xl font-medium mt-14 ml-24 bg-windmill z-20 absolute">
-        <h1>ENERGY</h1>
-        <h1 className="ml-12">CONSULTANCY</h1>
-        <h1 className="ml-36">SERVICES</h1>
-      </div>
-      <h1 className="text-gray-500 text-4xl font-bold p-10 ml-12 ">
-        "FOR YOUR BETTER
-        <br /> <span className="text-green-900">TOMORROW</span>, SAVE <br />
-        ENERGY <span className="text-green-200">TODAY</span>"
-      </h1>
-      <div className="pl-10 ml-12">
-        <button className="bg-green-300 rounded-3xl p-3">Know More</button>
+      <div className="absolute">
+        <div className="text-3xl font-medium mt-14 ml-24 bg-windmill z-20 absolute">
+          <h1>ENERGY</h1>
+          <h1 className="ml-12">CONSULTANCY</h1>
+          <h1 className="ml-36">SERVICES</h1>
+        </div>
+        <h1 className="text-gray-500 text-4xl font-bold p-10 ml-12 mt-40 ">
+          "FOR YOUR BETTER
+          <br /> <span className="text-green-900">TOMORROW</span>, SAVE <br />
+          ENERGY <span className="text-green-200">TODAY</span>"
+        </h1>
+        <div className="pl-10 ml-12">
+          <button className="bg-green-300 rounded-3xl p-3">Know More</button>
+        </div>
       </div>
     </div>
   );
